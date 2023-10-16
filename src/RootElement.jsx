3237ec2c9f853990f113d04bb5ecbdf407459cdf
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2023-10-16 19:03:22 +0600

    Basic Setup: Install tailwind css and daisy ui, react router, font

---
+++ src/RootElement.jsx
@@ -0,0 +1,9 @@
+const RootElement = () => {
+    return (
+        <div>
+            <h1 className="font-rancho">Hello this is root</h1>
+        </div>
+    );
+};
+
+export default RootElement;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {RouterProvider} from 'react-router-dom';
 import './index.css'
+import MainRoutes from './routes/MainRoutes';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={MainRoutes}></RouterProvider>
   </React.StrictMode>,
 )
