047d1f0b19ce6d56956f880fabeffb167b738dcd
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2023-10-17 03:04:32 +0600

    Feat: Add new coffee page - coffee data send to the sever and show success

--- src/RootElement.jsx
+++ src/RootElement.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Header/Navbar";
 import Footer from "./components/Footer/Footer";
+import { Toaster } from "react-hot-toast";
 
 const RootElement = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Toaster position="top-center" reverseOrder={false} />
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default RootElement;
--- src/pages/AddCoffee.jsx
+++ src/pages/AddCoffee.jsx
@@ -1,7 +1,37 @@
 import { Link } from "react-router-dom";
 import { IoMdArrowBack } from "react-icons/io";
+import axios from "axios";
+import toast from 'react-hot-toast';
+
 
 const AddCoffee = () => {
+    const handleAddCoffee = event => {
+        event.preventDefault()
+        const form = event.target;
+        const name = form.name.value;
+        const chef = form.chef.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+        const formData = {name, chef, supplier, taste, category, details, photo}
+        axios.post('http://localhost:5000/coffees',formData, {
+            headers: {
+                'content-type': 'application/json'
+            }
+        })     
+        .then(res => {
+            if(res.data.acknowledged){
+                console.log('success');
+                toast.success('Successfully Added!')
+
+            }
+        })
+        .catch(error => {
+            toast.error(error.message)
+          });
+    }
   return (
     <div
       style={{
@@ -15,7 +45,7 @@ const AddCoffee = () => {
         </Link>
 
         <div>
-          <form className="bg-[#F4F3F0] py-14 md:px-20 my-11 px-2 rounded-md">
+          <form onSubmit={handleAddCoffee} className="bg-[#F4F3F0] py-14 md:px-20 my-11 px-2 rounded-md">
             <div className="text-center">
               <h3 className="font-rancho text-5xl text-[#374151] drop-shadow-text">
                 Add New Coffee
@@ -109,13 +139,13 @@ const AddCoffee = () => {
                   type="text"
                   placeholder="Enter photo URL"
                   className="p-2 my-4 rounded-md outline-none w-full"
-                  name="details"
-                  id="details"
+                  name="photo"
+                  id="photo"
                 />
               </div>
               <input
                 type="submit"
-                className=" py-2 px-4 bg-primary text-secondery font-rancho text-2xl rounded-md border-2 border-secondery md:col-span-2"
+                className=" cursor-pointer py-2 px-4 bg-primary text-secondery font-rancho text-2xl rounded-md border-2 border-secondery md:col-span-2"
                 value="Add Coffee"
               />
             </div>
